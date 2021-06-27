a7c7a2583db4cbcb0f3e9385c63f3f84968077d6
KekoYuuko <mrkolos500@gmail.com>
2021-06-27 19:42:17 +0200

    27.06 Dk

--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,4 @@
+from re import S
 from app import app
 from app.models.opinion import Opinion
 from app.utils import extractElement
@@ -5,6 +6,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import json
+import matplotlib.pyplot as plt
 
 class Product:
 
@@ -14,14 +16,22 @@ class Product:
     def __init__(self, productId=None, name=None, opinions=[], averageScore=None, opinionsCount=None, prosCount=None, consCount=None):
         self.productId = productId
         self.name = name
-        self.opinions = opinions
+        self.opinions = opinions.copy()
         self.averageScore = averageScore
         self.opinionsCount = opinionsCount
         self.prosCount = prosCount
         self.consCount = consCount
-    
+
     def opinionsPageUrl(self):
         return self.url_pre+'/'+self.productId+self.url_post
+
+    def extractName(self):
+        respons = requests.get(self.opinionsPageUrl())
+        if respons.status_code == 200:
+            pageDOM = BeautifulSoup(respons.text, 'html.parser')
+            self.name = extractElement(
+                pageDOM, 'h1.product-top__product-info__name')
+        return self.name
     
     def extractProduct(self):
         url = self.opinionsPageUrl()
@@ -35,22 +45,27 @@ class Product:
                 url = self.url_pre + extractElement(pageDOM, 'a.pagination__next', "href") 
             except TypeError:
                 url = None
+
     def countProductStatistics(self):
         opinions = self.opinionsToDataFrame()
-        averageScore = float(opinions['stars'].mean())
-        opinionsCount = len(self.opinions)
-        prosCount = int(opinions['advantages'].count())
-        consCount = int(opinions['disadvantages'].count())
+        self.averageScore = float(opinions['stars'].mean())
+        self.opinionsCount = len(self.opinions)
+        self.prosCount = int(opinions['advantages'].count())
+        self.consCount = int(opinions['disadvantages'].count())
 
     def exportProduct(self):
+        with open("app/products/{}.json".format(self.productId), "w", encoding="UTF-8") as jf:
+            json.dump(self.productToDict(), jf,
+                      indent=4, ensure_ascii=False)
         with open("app/opinions/{}.json".format(self.productId), "w", encoding="UTF-8") as jf:
-            json.dump(self.toDict(), jf, indent=4, ensure_ascii=False)
+            json.dump(self.opinionsToDictsList(), jf, indent=4, ensure_ascii=False)
 
     def importProduct(self):
-        with open("app/opinions/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
+        with open("app/products/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
             product = json.load(jf)
-            self.name = product['name']
-            opinions = product['opinions']
+            self.__init__(**product)    
+        with open("app/opinions/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
+            opinions = json.load(jf)
             for opinion in opinions:
                 self.opinions.append(Opinion(**opinion))
         return self
@@ -60,22 +75,8 @@ class Product:
         name: {}<br>'''.format(self.productId, self.name)+"<br>".join(str(opinion) for opinion in self.opinions)
 
     def toDict(self):
-        return (self.productToDict(){"opinions": self.opinionsToDictsList()})
+        return (self.productToDict()|{"opinions": self.opinionsToDictsList()})
 
-    def opinionsToDataFrame(self):
-        opinions = pd.DataFrame.from_records(
-            [opinion.toDict() for opinion in self.opinions])
-        # opinions = pd.json_normalize([opinion.toDict() for opinion in self.opinions])
-        return opinions
-    
-    def extractName(self):
-        respons = requests.get(self.opinionsPageUrl())
-        if respons.status_code == 200:
-            pageDOM = BeautifulSoup(respons.text, 'html.parser')
-            self.name = extractElement(
-                pageDOM,'h1.product-top_product-info_name')
-        return self.name
-    
     def productToDict(self):
         return {
             "productId": self.productId,
@@ -85,6 +86,42 @@ class Product:
             "prosCount": self.prosCount,
             "consCount": self.consCount
         }
+
     def opinionsToDictsList(self):
         return [opinion.toDict() for opinion in self.opinions]
 
+    def opinionsToDataFrame(self):
+        # opinions = pd.DataFrame.from_records(
+            # [opinion.toDict() for opinion in self.opinions])
+        opinions = pd.json_normalize([opinion.toDict() for opinion in self.opinions])
+        return opinions
+
+    def getSummaryData(self):
+        with open("app/opinions/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
+            opinions = {}
+            json_data = json.load(jf)
+
+            for op in json_data:
+                star = str(op['stars'])
+                opinion = int(opinions.get(star,0))
+                opinion += 1
+                opinions.update({star: opinion})
+
+        return opinions
+
+    def getTruFalseOpitions(self):
+        with open("app/opinions/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
+            opinions = {}
+            json_data = json.load(jf)
+
+            for op in json_data:
+                recomendation = str(op['recommendation'])
+                opinion = int(opinions.get(recomendation,0))
+                opinion += 1
+                opinions.update({recomendation: opinion})
+        return opinions
+
+
+
+
+    
--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,11 @@ from app.models.opinion import Opinion
 from app.models.product import Product
 from app.forms import ProductForm
 from os import listdir
+import pandas as pd
 import requests
 import json
 
-app.config['SECRET_KEY'] = "YeahIGuessSo"
+app.config['SECRET_KEY'] = "NotSoSecretKey"
 
 @app.route('/')
 @app.route('/index')
@@ -19,26 +20,35 @@ def extract():
     form = ProductForm()
     if request.method == 'POST' and form.validate_on_submit():
         product = Product(request.form['productId'])
-        respons = requests.get(product.opinionsPageUrl())
-        if respons.status_code == 200:
+        if (product.extractName()):
             product.extractProduct()
+            product.countProductStatistics()
             product.exportProduct()
             return redirect(url_for('product', productId=product.productId))
         else:
             form.productId.errors.append("For given productId there is no product")
     return render_template('extract.html.jinja', form=form)
 
-@app.route('/product/<productId>')
+@app.route('/<productId>')
 def product(productId):
     product = Product(productId)
     opinions = product.importProduct().opinionsToDataFrame()
-    return render_template('product.html.jinja', tables=[opinions.to_html(classes='table table-striped table-sm table-responsive', table_id="opinions")])
+    opinionBar = product.getSummaryData()
+    opinionPie = product.getTruFalseOpitions()
+    return render_template('product.html.jinja', tables=[opinions.to_html(classes='table table-striped table-sm table-responsive', table_id="opinions")],opinionBar=opinionBar, opinionPie=opinionPie)
 
 @app.route('/products')
 def products():
-    productsList = [x.split(".")[0] for x in  listdir("app/opinions")]
-    return render_template('products.html.jinja', productsList=productsList)
+    productsList = [x.split(".")[0] for x in  listdir("app/products")]
+    productsDictsList = []
+    for product in productsList:
+        with open("app/products/{}.json".format(product), "r", encoding="UTF-8") as jf:
+            productsDictsList.append(json.load(jf))
+    # products = pd.json_normalize(productsDictsList)
+    return render_template('products.html.jinja', products=productsDictsList)
+
+
 
 @app.route('/author')
 def author():
-    pass
+    return render_template('author.html.jinja')
